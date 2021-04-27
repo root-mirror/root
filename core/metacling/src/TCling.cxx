0a1895fe06748cbfdb77abdf708efa88256ba63b
Philippe Canal <pcanal@fnal.gov>
2021-04-27 12:50:25 -0500

    Fix compilation error in gSystem Load call core/metacling/src/TCling.cxx

--- core/metacling/src/TCling.cxx
+++ core/metacling/src/TCling.cxx
@@ -7206,7 +7206,7 @@ static std::string GetSharedLibImmediateDepsSlow(std::string lib,
                continue;
 
             Result += cand + ' ';
-            gSystem->Load(cand);
+            gSystem->Load(cand.c_str());
          }
       }
    }
