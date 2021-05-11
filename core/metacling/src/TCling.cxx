a1ffcf2f8d0c756efd2bffb19afca932557dbc54
Axel Naumann <Axel.Naumann@cern.ch>
2021-05-11 18:11:08 +0200

    [metacling] Make TClingMethodInfo::NormalizedName() a proper interface.

--- core/metacling/src/TCling.cxx
+++ core/metacling/src/TCling.cxx
@@ -9084,10 +9084,7 @@ const char* TCling::MethodInfo_TypeName(MethodInfo_t* minfo) const
 std::string TCling::MethodInfo_TypeNormalizedName(MethodInfo_t* minfo) const
 {
    TClingMethodInfo* info = (TClingMethodInfo*) minfo;
-   if (info && info->IsValid())
-      return info->Type()->NormalizedName(*fNormalizedCtxt);
-   else
-      return "";
+   return info->NormalizedName(*fNormalizedCtxt);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
