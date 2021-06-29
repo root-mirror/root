c611fd14dc2e697bdf64341a044202bae1ea44af
Jonas Rembser <jonas.rembser@cern.ch>
2021-06-29 19:39:55 +0200

    [RF] Separate buffer for scalar results in RooFitDriver
    Also includes some general refactoring of the RooFitDriver.

--- roofit/roofitcore/inc/RooFitDriver.h
+++ roofit/roofitcore/inc/RooFitDriver.h
@@ -7,6 +7,7 @@
 #include <unordered_map>
 
 class RooAbsData;
+class RooAbsArg;
 class RooAbsReal;
 class RooArgSet;
 class RooNLLVarNew;
@@ -19,20 +20,31 @@ class RooFitDriver {
      double getVal();
      RooArgSet* getParameters();
      
+    struct NodeInfo {
+        int nServers = 0;
+        int nClients = 0;
+    };
+
   private:
+
+    double* getAvailableBuffer();
+
     const int batchMode=0;
     double* cudaMemDataset=nullptr;
     // used for preserving static info about the computation graph
     rbc::DataMap dataMap;
-    const RooArgSet* observables=nullptr;
     const RooNLLVarNew& topNode;
     size_t nEvents;
+
+    RooAbsData const* _data = nullptr;
+
     std::queue<const RooAbsReal*> initialQueue;
-    std::unordered_map<const RooAbsReal*,std::pair<int,int>> nServersClients;
+
+    std::unordered_map<const RooAbsArg*,NodeInfo> _nodeInfos;
     // used for dynamically scheduling each step's computations
     std::queue<const RooAbsReal*> computeQueue;
-    std::unordered_map<const RooAbsReal*,std::pair<int,int>> nRemainingServersClients;
-    std::queue<double*> buffers;
+
+    std::queue<double*> _vectorBuffers;
 };
 
 #endif //ROO_FIT_DRIVER_H
