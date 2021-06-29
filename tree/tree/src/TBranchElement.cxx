66445a89da08b0563f76cf7d4bb5146d7213b440
Philippe Canal <pcanal@fnal.gov>
2021-06-29 16:34:29 -0500

    TBranchElement::InitializeOffsets: don't confuse top level collection for missing data member

--- tree/tree/src/TBranchElement.cxx
+++ tree/tree/src/TBranchElement.cxx
@@ -3591,7 +3591,8 @@ void TBranchElement::InitializeOffsets()
             // to remove it's name to get the correct real data offsets
             ////////////////////////////////////////////////////////////////////
 
-            if( dynamic_cast<TBranchSTL*>(fParent) && stlParentName.Length() )
+            const bool isToplevelCollection = (this == GetMother() && (fType == 3 || fType == 4));
+            if( (dynamic_cast<TBranchSTL*>(fParent) || isToplevelCollection) && stlParentName.Length() )
             {
                if( !strncmp( stlParentName.Data(), dataName.Data(), stlParentName.Length()-1 )
                   && dataName[ stlParentName.Length() ] == '.' )
